25406dc99479033f342d224dcbe292cac571f31f
Lukas Toth <lukas@lukastoth.com>
2009-08-11 19:41:43 -0400

    Changed order of .gitconfig generation for Win32

--- install.cmd
+++ install.cmd
@@ -4,5 +4,5 @@ copy /Y vimrc "%USERPROFILE%\_vimrc"
 copy /Y gvimrc "%USERPROFILE%\_gvimrc"
 
 copy /Y gitignore "%USERPROFILE%\.gitignore"
-type "%USERPROFILE%\.gitconfig.local" > "%USERPROFILE%\.gitconfig"
-type gitconfig >> "%USERPROFILE%\.gitconfig"
+type gitconfig > "%USERPROFILE%\.gitconfig"
+type "%USERPROFILE%\.gitconfig.local" >> "%USERPROFILE%\.gitconfig"
